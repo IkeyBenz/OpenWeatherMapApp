cad702835e68508cd942141d12c65bb0be55eb73
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-13 23:36:01 -0700

    [cleanup] added comments

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+require('./database/db');
 
 const app = express();
 
 const { PORT } = process.env;
 
+// Allow full access to static client files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Allow submission of form data
+app.use(express.urlencoded({ extended: false }));
+
+// Listen for weather or mood requests
 app.use(require('./controllers/openweather'));
+app.use(require('./controllers/moods'));
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
